bb84a3bc4f97597251006714a66c1479fd570837
Lorenzo Camilli <lorcamilli@gmail.com>
2022-02-27 18:02:48 +0100

    Aggiunto button_handler per gestione bottoni

--- main.py
+++ main.py
@@ -7,20 +7,12 @@ from dotenv import load_dotenv
 import logging
 from telegram.update import Update
 from telegram.ext import (Updater, CallbackContext, CommandHandler,
-                          MessageHandler, ConversationHandler, Filters, CallbackContext)
+                          MessageHandler, ConversationHandler, Filters, CallbackContext,  CallbackQueryHandler)
 from telegram import (InlineKeyboardButton, InlineKeyboardMarkup)
 
 load_dotenv()
 
 TOKEN = os.getenv('TESTTOKEN')
-
-
-
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO
-)
-
-logger = logging.getLogger(__name__)
 
 
 file_path = './json/frasi.json'
@@ -34,16 +26,13 @@ else:
 
 
 def start(update: Update, context: CallbackContext):
-    user = update.message.from_user
-    text = str(frasi["start"])
 
-    buttons = [[InlineKeyboardButton(str(frasi["button_start"]), callback_data='1')], [InlineKeyboardButton(str(frasi["button_start2"]), callback_data='2')]]
-    
+    buttons = [[InlineKeyboardButton(str(frasi["button_start"]), callback_data="help")], [
+        InlineKeyboardButton(str(frasi["button_start2"]), callback_data="supporto")]]
+
     reply_markup = InlineKeyboardMarkup(buttons)
-    update.message.reply_markdown(text)
+    update.message.reply_markdown(str(frasi["start"]))
     update.message.reply_text(str(frasi["start2"]), reply_markup=reply_markup)
-
-
 
 
 def help(update: Update, context: CallbackContext):
@@ -52,6 +41,24 @@ def help(update: Update, context: CallbackContext):
 
 def unknown(update: Update, context: CallbackContext):
     update.message.reply_text(frasi["comando_non_riconosciuto"])
+
+
+def buttons_handler(update: Update, context: CallbackContext):
+    query = update.callback_query
+    query.answer()
+    if str(query.data) == "help":
+        query.message.reply_markdown(str(frasi["cmd_help"]))
+    elif str(query.data) == "supporto":
+        buttons = [[InlineKeyboardButton(str(frasi["button_support"]), url="https://t.me/joinchat/BCql3UMy26nl4qxuRecDsQ")], [InlineKeyboardButton(str(frasi["button_support2"]),callback_data="forum")], [InlineKeyboardButton(str(frasi["button_support3"]), url="https://forum.mozillaitalia.org/index.php?board=9.0")], [InlineKeyboardButton(str(frasi["button_back_mostra_help"]), callback_data="help")]]
+       
+        reply_markup = InlineKeyboardMarkup(buttons)
+        query.message.reply_markdown(str(frasi["cmd_supporto"]),  reply_markup=reply_markup)
+    elif str(query.data) == "forum":
+        buttons = [[InlineKeyboardButton(str(frasi["button_forum"]),
+            url="https://forum.mozillaitalia.org/")], [InlineKeyboardButton(str(frasi["button_back_mostra_help"]), callback_data="help")]]
+
+        reply_markup = InlineKeyboardMarkup(buttons)
+        query.message.reply_markdown(str(frasi["forum"]),  reply_markup=reply_markup)
 
 
 def main() -> None:
@@ -63,6 +70,7 @@ def main() -> None:
     dp.add_handler(CommandHandler('help', help))
     dp.add_handler(MessageHandler(Filters.text, unknown))
     dp.add_handler(MessageHandler(Filters.command, unknown))
+    dp.add_handler(CallbackQueryHandler(buttons_handler))
 
     updater.start_polling()
     updater.idle()
