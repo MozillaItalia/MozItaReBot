7276361c23b9aa5cbf8e5f24f04f16a7add3d7fb
Lorenzo Camilli <lorcamilli@gmail.com>
2022-03-06 18:20:59 +0100

    Aggiunto handler per bottone progetti

--- main.py
+++ main.py
@@ -166,6 +166,30 @@ def buttons_handler(update: Update, context: CallbackContext):
         query.message.reply_markdown(
             str(frasi["forum"]),  reply_markup=reply_markup)
 
+    elif str(query.data).lower() == "progetti":
+        buttons = []
+        for nome_prog_moz in liste["progetti"]:
+
+            buttons.append([InlineKeyboardButton(
+                nome_prog_moz, callback_data="progetti", url=liste["progetti"][str(nome_prog_moz)])])
+
+        reply_markup = InlineKeyboardMarkup(buttons)
+        query.message.reply_text(
+            str(frasi["cmd_progetti"]), reply_markup=reply_markup)
+
+        buttons.clear()
+
+        for nome_prog_mozita in liste["progetti_mozita"]:
+
+            buttons.append([InlineKeyboardButton(
+                nome_prog_mozita, callback_data="progetti", url=liste["progetti_mozita"][str(nome_prog_mozita)])])
+
+        buttons.append([InlineKeyboardButton(
+            str(frasi["button_back_mostra_help"]),    callback_data="help")])
+
+        query.message.reply_text(
+            str(frasi["cmd_progetti2"]), reply_markup=reply_markup)
+
 
 def main() -> None:
 
